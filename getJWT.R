f3a3274b512172fc87c0d22faa7caa181e8972f9
juan <>
2023-04-14 08:03:20 -0300

    agregado roxygen para getJWT

--- getJWT.R
+++ getJWT.R
@@ -1,3 +1,22 @@
+#' Crea un JWT para integración con Ghost.or
+#' 
+#' Toma el secret de la variable de entorno GHOST_SECRET_ADMIN
+#' y crea un JWT para integración con Ghost.
+#' 
+#' @details
+#' El secret se obtiene de la variable de entorno GHOST_SECRET_ADMIN
+#' que se setea en el archivo .Renviron
+#' el nombre en el archivo es GHOST_SECRET_ADMIN o
+#' GHOST_SECRET_[USER]
+#' La función toma el valor de la variable de entorno y lo divide en dos partes
+#' separadas por ":". La primera parte es el kid y la segunda el secret.
+#' 
+#' @param integration Qué variable tomará. Default es "admin"
+#' @return Devuelve un JWT para integración con Ghost
+#' @examples getJWT("admin") -> Devuelve un JWT para integración con Ghost
+#' @export
+#' @importFrom jose jwt_encode_hmac jwt_claim
+#' @importFrom wkb hex2raw
 getJWT = function(integration = "admin") {
   require(jose)
   require(wkb)
